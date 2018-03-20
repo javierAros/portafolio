1ad741afea71eb918a49260aca31b2cd95a22450
Javier Aros <javier.arosmendoza@gmail.com>
2018-03-20 00:06:11 -0300

    agregado firebase al portafolio

--- src/app/components/portafolio/portafolio.component.ts
+++ src/app/components/portafolio/portafolio.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ProductosService} from "../../services/productos.service";
+import { AppComponent } from "../../app.component";
 
 @Component({
   selector: 'app-portafolio',
   templateUrl: './portafolio.component.html'
 })
-export class PortafolioComponent {
+export class PortafolioComponent extends AppComponent{
+  
 
 }
