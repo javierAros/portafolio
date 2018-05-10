ea19e3fe7db0e74c75922d5b5141afab5c655897
Javier Aros <javier.arosmendoza@gmail.com>
2018-05-09 23:46:01 -0300

    termino de arreglos version 1

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -9,7 +9,7 @@ export class SearchComponent {
   termino:string = undefined;
 
   constructor(private route:ActivatedRoute,
-              private _ps:ProductosService) {
+              public _ps:ProductosService) {
     route.params.subscribe( parametros=>{
       this.termino = parametros['termino'];
       _ps.buscar_producto(this.termino);
